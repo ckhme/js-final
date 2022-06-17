3032ce940f8d8dd9872d9ae0eaf4b50031f24989
ckhme <76675622+ckhme@users.noreply.github.com>
2022-06-17 22:48:18 +0900

    Update weather.js

--- src/app/weather.js
+++ src/app/weather.js
@@ -5,8 +5,6 @@ const onSuccess = async (pos)=>{
     const {coords : { latitude : lat, longitude : lng } } = pos;
     const res =await fetch(`${WEATHER_URL}weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`);
     const result = await res.json(); 
-    // console.log(result.name, result.weather[0].main );  
-    console.log(result );  
     document.querySelector("#top-left").innerText = result.name;
     document.querySelector("#top-right").innerText = `${Math.round(result.main.temp)}℃ / ${result.weather[0].main}`;
 }
